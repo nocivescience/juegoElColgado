790231f5b5391005cff998362266adaa1198ef42
nocive.science <nocive.science@gmail.com>
2023-10-23 05:36:11 -0300

    se ven las letras de la palabra

---
+++ practica1.js
@@ -0,0 +1,13 @@
+// const palabra= ["hola", "como", "estas", "bien", "adios"];
+// console.log(typeof(palabra));
+// console.log(typeof(palabra.join('')));
+// const lista1 = {};
+// const lista2 = [];
+// console.log(lista1==lista2);
+
+const palabra= 'paragua'
+console.log(palabra.includes('chile'));
+const lista1 = [1,2,3,4];
+console.log(lista1.reduce((a,b)=>b/a));
+console.log(palabra.replace(/a/g, 'o'))
+console.log(palabra);
